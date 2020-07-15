e349724487f4a7b11d5443222ba1557ec3d3575f
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-07-15 23:11:55 +0300

    Create_1.rb_lasson_9

---
+++ 09/1.rb
@@ -0,0 +1,11 @@
+@studenrs = ["Deineko Misha", "Sergey Brin", "Stephen Gary Wozniak", "Bill Geyts", "Stiv Jobs", "Lary Page", "Elon Musk"]
+
+
+
+def print_all_students
+    @studenrs.each do |student|
+        puts student
+    end
+end    
+
+print_all_students
