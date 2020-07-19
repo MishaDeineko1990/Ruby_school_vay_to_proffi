908f6a4aed675e1678faf5870376a361026b7620
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-07-19 23:55:52 +0300

    cut_leterals_ellements_in_arrey

--- 10/1.rb
+++ 10/1.rb
@@ -1,6 +1,14 @@
+# persson=%w[misha sergiy valia natasha dania vasia tania olia bogdan alla]
+# num=1
+# persson.each do |ell ll|
+#     puts "#{ll}: #{ell[1..-2]}."
+#     num=num+1
+# end
+
 persson=%w[misha sergiy valia natasha dania vasia tania olia bogdan alla]
+persson=persson[1..-2]
 num=1
-persson.each do |ell ll|
-    puts "#{ll}: #{ell[1..-2]}."
+persson.each do |ell|
+    puts "#{num}: #{ell}."
     num=num+1
 end
