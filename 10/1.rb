2f96b44459b7c4af59b5d0e280a6b47164b1dae1
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-07-20 23:15:10 +0300

    Update 1.rb

--- 10/1.rb
+++ 10/1.rb
@@ -3,12 +3,40 @@
 # persson.each do |ell ll|
 #     puts "#{ll}: #{ell[1..-2]}."
 #     num=num+1
+ # end
+
+# persson=%w[misha sergiy valia natasha dania vasia tania olia bogdan alla]
+# persson=persson[1..-2]
+# num=1
+# persson.each do |ell|
+#     puts "#{num}: #{ell}."
+#     num=num+1
 # end
 
-persson=%w[misha sergiy valia natasha dania vasia tania olia bogdan alla]
-persson=persson[1..-2]
-num=1
-persson.each do |ell|
-    puts "#{num}: #{ell}."
-    num=num+1
-end
+
+arr = %w[roman miki 32 ror git oll]
+
+puts "arr[1...-1]"
+puts arr[1...-1]
+
+puts
+puts "==============="
+puts
+
+puts "arr[1..-2]"
+puts arr[1..-2]
+
+
+puts
+puts "==============="
+puts
+
+puts "arr[1...-1][0]"
+puts arr[1...-1][0]
+
+puts
+puts "==============="
+puts
+
+puts "arr[1...-1][0][1...-1]"
+puts arr[1...-1][0][1...-1]
