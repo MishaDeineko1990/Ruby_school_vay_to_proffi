1ce16f28060ffdb0306a5d722c6ce3ae2457aec7
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-07-22 00:35:26 +0300

    Update 3.rb

--- 10/3.rb
+++ 10/3.rb
@@ -1,19 +1,27 @@
 arr = []
 
 loop do
+
     puts "Who add to arrey"
     add_name = gets.chomp
 
     if add_name==""
         
-        puts arr 
+        x=1 
+
+        arr.each do |el|
+            puts "#{x}: #{el}" 
+            x=x+1
+        end
+        
         exit
 
     else
 
         arr.push add_name
         puts "you add items in arrrey: (\"#{arr.last}\")"
-
+        puts
 
     end
+
 end
