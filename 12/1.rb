64ecc59aff1ef1ec7ac8182f14247d416df7c2d2
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-08-02 21:44:07 +0300

    lasson hesh continuation

---
+++ 12/1.rb
@@ -0,0 +1,39 @@
+hh = {}
+
+hh [:a] = 1 
+
+hh [:b] = 2 
+
+hh [:c] = 3 
+
+hh [:d] = 4 
+
+hh2={:a=>1,:b=>2,:c=>3,:d=>4,:e=>5}
+
+puts "valyes hesh #{hh.values}"
+
+puts
+
+puts "keys hesh #{hh.keys}"
+
+puts "======================="
+
+n_value = 0
+n_value2 = 0
+
+hh.each_value do ||
+    n_value = n_value+1 
+end
+
+
+puts n_value
+
+puts "hh3 nums values equal"
+
+hh2.each_value do ||
+    n_value2 = n_value2+1 
+end
+
+puts n_value2
+
+puts "keys hesh2 #{hh2.keys}"
