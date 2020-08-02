4934f0022e05ee5621bafb947bdfdae3dcb103d1
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-08-02 17:29:26 +0300

    oll about hash

--- 11/3.rb
+++ 11/3.rb
@@ -3,4 +3,42 @@ hashhh = {
     :mila => "333333"
 }
 
+
+
+puts hashhh
+
+puts
+
 puts hashhh[:mike][:age]
+
+
+
+
+
+
+puts
+
+hashhh [:sara] = {:phone => "34343", :age => "28",:profesia =>"designer"}
+
+puts hashhh
+
+puts hashhh[:sara][:age]
+
+
+
+
+
+
+aa = :Svitlana
+
+bb = {:phone => "33456", :age => "20",:profesia =>"student"}
+
+hashhh [aa] = bb
+
+puts
+
+puts hashhh
+
+puts hashhh[aa][:age]
+
+puts "end"
---
+++ 11/4.rb
@@ -0,0 +1,4 @@
+h = {:a => 1, :b => 2.2}
+h.each_with_index do |(k, v), i|
+  p k, v, i
+end
