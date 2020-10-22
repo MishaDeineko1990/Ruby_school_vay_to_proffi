b2d3bcf370c606546c921984f7bcab8e7e303318
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-10-22 21:37:27 +0300

    I'm tired

---
+++ 18/passwords.rb
@@ -0,0 +1,21 @@
+input = File.open("passwords.txt", "r")
+
+while (true)
+
+    def check
+        print "Write your password for a check on security: "
+        a = gets.chomp!
+        break if a==""
+        while (line=input.gets)
+
+
+
+        end
+        p ""
+        print "if you want "
+    end
+
+    
+
+    check
+end
