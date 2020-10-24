5653be746c3f15fd37e530b83385f385e3de937b
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-10-24 08:35:22 +0300

    finish chack password

--- 18/passwords.rb
+++ 18/passwords.rb
@@ -1,21 +1,39 @@
-input = File.open("passwords.txt", "r")
 
-while (true)
+    @a = ""
+    @check_prog = false
+
+    def inner_p        
+        print "Write your password for a check on security (input enter what cloce programm): "        
+        @a = gets.chomp!
+        exit if @a==""
+        check
+    end
 
     def check
-        print "Write your password for a check on security: "
-        a = gets.chomp!
-        break if a==""
+        input = File.open("passwords.txt", "r")
         while (line=input.gets)
-
-
-
+            
+            if line.chomp == @a
+                p "#{line.chomp}  -   #{@a.chomp}"
+                @check_prog = true 
+                break
+            end
+                        
         end
-        p ""
-        print "if you want "
+        write_resault
+    end
+
+    def write_resault
+        if @check_prog
+            p "You password is not have fine security"
+        else
+            p "You password is fine"
+        end
+        inner_p
     end
 
     
 
-    check
-end
+    inner_p
+    
+
