c62e124a9ce1b0c1b97baaf8daf49b02b84059da
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-10-30 23:40:16 +0200

    20_2 barber_SHop

--- 20/barbershop/barber.rb
+++ 20/barbershop/barber.rb
@@ -3,3 +3,18 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+post "/" do
+    @user_name = params[:user_name]
+    @phone = params[:phone]
+    @date_time = params[:date_time]
+
+    @title = 'thenk You'
+    @message = "deer #{@user_name}, we`ll be witing for you at #{@date_time}"
+
+    f = File.open 'user.txt', 'a'
+    f.write "User #{@user_name}, phone: #{@phone}, Date and time #{@date_time}  ===========   "
+    f.close
+
+    erb :message
+end
