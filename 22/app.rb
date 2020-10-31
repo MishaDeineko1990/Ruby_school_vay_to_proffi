35308b2a5c28ccaf26211e589d1074e7cc5d3a17
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-11-01 01:18:20 +0200

    finish 22

--- 22/app.rb
+++ 22/app.rb
@@ -10,3 +10,20 @@ end
 get '/about' do
 	erb :about
 end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@name = params[:name]
+	@phone = params[:phone]
+	@data = params[:data]
+
+	f= File.open 'views/list.erb', 'a'
+    f.write "User #{@name}, phone: #{@phone}, Date and time #{@data} <br/>   "
+    f.close
+
+	erb "Thank You weew well for you."
+
+end
