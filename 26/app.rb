0ca68d459632d50ef9768510bcf93a486d9c6d61
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-11-04 11:06:07 +0200

    26_start

---
+++ 26/app.rb
@@ -0,0 +1,51 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	@x = def is_a_valid_email?(email)
+	(email =~ /^(([A-Za-z0-9]*\.+*_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\+)|([A-Za-z0-9]+\+))*[A-Z‌​a-z0-9]+@{1}((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,4}$/i)
+	end
+
+	hh = {
+		:username => "Enter You name",
+		:phone => "Enter phone",
+		:datetime => "Enter date and time"
+	}
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+
+
+
+	erb "Ok, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+end
+
+# https://stackoverflow.com/questions/33557817/how-to-send-mail-with-sinatra-using-pony
+
