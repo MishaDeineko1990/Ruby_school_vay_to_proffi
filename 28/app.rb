afaa0d79ee707779dc6618dfb9c2a2af1a5ae37f
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-11-06 16:53:48 +0200

    28_almost_midle
    28_almost_midle

--- 28/app.rb
+++ 28/app.rb
@@ -3,6 +3,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def innit_db
+	@db = SQLite3::Database.new
+	@db.results_as_hash = true
+end
+
+before do
+	innit_db
+end
+
 get '/' do
 	erb "HAllo on Liprozorium"
 end
@@ -11,4 +20,10 @@ end
 
 get '/new' do
 	erb :new
+end
+
+post '/new' do
+	@content = params[:content]
+
+	erb "You typed #{@content}"
 end
