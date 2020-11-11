2daa23d2c1b9fd75c80ab538028c254cf3ee8893
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-11-11 06:23:49 +0200

    update

---
+++ 29/sss/app.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db" 
+
+class Client < ActiveRecord::Base
+    
+end
+
+get "/" do
+    erb "dddd"
+end
