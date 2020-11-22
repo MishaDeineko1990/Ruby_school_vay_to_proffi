59107aae9d8a46cb8c701ea231cbf5564cf5e442
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2020-11-22 22:18:34 +0200

    Finish_lasson_30

--- 30/app.rb
+++ 30/app.rb
@@ -6,6 +6,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, presents: true, length: {minimum: 3}
+	validates :phone, presents: true
+	validates :datestamp, presents: true
+	validates :color, presents: true
 end
 
 class Barber < ActiveRecord::Base
@@ -20,14 +24,32 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
+end
+
+get '/barber/:id' do
+	@barber = Barber.find params[:id]
+	erb :barber
+end
+
+get '/bookings' do
+	@Clients= Cliuent.order ('created_at DESC')
+	erb :bookings
+end
+
+get '/client/:id' do 
+	@client = Client.find(params[:id])
+	erb :client
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>Спасибо, вы записались!</h2>"
-
+	@c = Client.new params[:client]
+	if @c.save
+		erb "<h2>Спасибо, вы записались!</h2>"
+	else
+		@error = c.errors.full_messages.first
+		erb :visit
+	end
 end
