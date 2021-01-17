a752d0fcb96a2b06c8af25b01405a49eab802307
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-01-17 21:11:45 +0200

    refactoring_tests

--- 42/rspec/hero_spec.rb
+++ 42/rspec/hero_spec.rb
@@ -2,24 +2,24 @@ require './hero'
 
 describe Hero do 
 
-    it 'has a capitalize name' do
-        hero = Hero.new 'mike'
-        expect(hero.name).to eq 'Mike'
+    before do
+        @hero = Hero.new 'mike'
+    end
+
+    it 'has a capitalize name' do        
+        expect(@hero.name).to eq 'Mike'
     end
 
     it 'health_up is fine' do
-        hero = Hero.new 'mike'
-        expect(hero.health_up).to eq 110
+        expect(@hero.health_up).to eq 110
     end
 
     it 'health_down is fine' do
-        hero = Hero.new 'mike'
-        expect(hero.health_down).to eq 90
+        expect(@hero.health_down).to eq 90
     end
 
     it 'health_info is fine' do
-        hero = Hero.new 'mike'
-        expect(hero.hero_info).to eq 'Mike has a health of 100'
+        expect(@hero.hero_info).to eq 'Mike has a health of 100'
     end
 
 end
